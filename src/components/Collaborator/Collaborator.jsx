a1bf38b11e49140feebd8d86fc85ea278cedb12f
athena272 <guilhermera272@gmail.com>
2024-02-05 10:37:33 -0300

    Create Collaborator.jsx

---
+++ src/components/Collaborator/Collaborator.jsx
@@ -0,0 +1,23 @@
+import styles from './Collaborator.module.scss'
+import PropTypes from "prop-types"
+
+Collaborator.propTypes = {
+    name: PropTypes.string.isRequired,
+    role: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    primaryColor: PropTypes.string.isRequired,
+}
+
+export default function Collaborator({ name, role, image, primaryColor }) {
+    return (
+        <div className={styles.collaborator}>
+            <div className={styles.header} style={{ backgroundColor: primaryColor }}>
+                <img src={image} alt={name} />
+            </div>
+            <div className={styles.footer}>
+                <h4>{name}</h4>
+                <h5>{role}</h5>
+            </div>
+        </div>
+    )
+}
