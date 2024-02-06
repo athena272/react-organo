d8d89d805a1964bc6884456a5fb2882ca33f304e
athena272 <guilhermera272@gmail.com>
2024-02-06 00:23:15 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
+import "@fontsource/poppins";
 import Banner from './components/Banner/Banner'
 import Form from './components/Form/Form'
 import Time from './components/Time/Time'
 import Footer from './components/Footer/Footer'
 import { times } from './data'
+import { useLocalState } from './hooks';
 
 export default function App() {
-  const [collaborators, setCollaborators] = useState([])
+  // const [collaborators, setCollaborators] = useState([])
+  const [collaborators, setCollaborators] = useLocalState("collaborators", [])
 
   function addCollaborators(collaborator) {
-    // setCollaborators(prevState => [...prevState, collaborator])
+    // setCollaborators([...collaborators, collaborator])
     // console.log(collaborators)
 
     setCollaborators(prevState => {
