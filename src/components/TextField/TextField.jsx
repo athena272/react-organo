c0f9b60342278f1ff9f88702135616523dfb3396
athena272 <guilhermera272@gmail.com>
2024-02-06 00:22:45 -0300

    Update TextField.jsx

--- src/components/TextField/TextField.jsx
+++ src/components/TextField/TextField.jsx
@@ -2,6 +2,7 @@ import styles from './TextField.module.scss'
 import PropTypes from "prop-types"
 
 TextField.propTypes = {
+    id: PropTypes.string.isRequired,
     label: PropTypes.string.isRequired,
     placeholderText: PropTypes.string.isRequired,
     isRequired: PropTypes.bool.isRequired,
@@ -9,17 +10,19 @@ TextField.propTypes = {
     onTyping: PropTypes.func.isRequired,
 }
 
-export default function TextField({ label, placeholderText, isRequired, valueToUse, onTyping }) {
+export default function TextField({ id, label, placeholderText, isRequired, valueToUse, onTyping }) {
     function handleTyping(event) {
         onTyping(event.target.value)
     }
 
     return (
         <div className={styles.textField}>
-            <label>
+            <label htmlFor={id}>
                 {label}
             </label>
             <input
+                aria-label={id}
+                id={id}
                 value={valueToUse}
                 onChange={handleTyping}
                 placeholder={placeholderText}
