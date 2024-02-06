06f2baea27a0bd740a85261b99b81ff0a5323ac0
athena272 <guilhermera272@gmail.com>
2024-02-06 01:00:12 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
+import { ViteMinifyPlugin } from 'vite-plugin-minify'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  esbuildbuild: {
-    minify: true,
-  },
-  plugins: [react()],
+  plugins: [
+    react(),
+    // input https://www.npmjs.com/package/html-minifier-terser options
+    ViteMinifyPlugin({}),
+  ],
 })
