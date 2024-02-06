3966a064bd56d1a9a28d7084c0f0e19fd0c1d9f4
athena272 <guilhermera272@gmail.com>
2024-02-05 22:41:23 -0300

    Update Banner.jsx

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,7 +4,13 @@ export default function Banner() {
 
     return (
         <header className={styles.banner}>
-            <img src="/assets/banner.png" alt="O banner principal da página do Organo" />
+            <img
+                src="/assets/banner.png"
+                alt="O banner principal da página do Organo"
+                rel="preload"
+                width={1440}
+                height={556}
+            />
         </header>
     )
 }
