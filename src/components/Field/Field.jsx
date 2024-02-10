f1068872f33f91a441b5f0def532c2215438a1be
athena272 <guilhermera272@gmail.com>
2024-02-10 14:24:32 -0300

    Update Field.jsx

--- src/components/Field/Field.jsx
+++ src/components/Field/Field.jsx
@@ -3,12 +3,12 @@ import PropTypes from "prop-types"
 
 Field.propTypes = {
     id: PropTypes.string.isRequired,
-    type: PropTypes.string.isRequired,
     label: PropTypes.string.isRequired,
     placeholderText: PropTypes.string.isRequired,
     isRequired: PropTypes.bool.isRequired,
     valueToUse: PropTypes.string.isRequired,
     onTyping: PropTypes.func.isRequired,
+    type: PropTypes.string,
 }
 
 export default function Field({ id, label, placeholderText, isRequired, valueToUse, onTyping, type = 'text' }) {
