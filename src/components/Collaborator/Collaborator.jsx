d02e3ee8e2a526c264c23b29a61b5aa96fa0a9dc
athena272 <guilhermera272@gmail.com>
2024-02-10 09:07:17 -0300

    Update Collaborator.jsx

--- src/components/Collaborator/Collaborator.jsx
+++ src/components/Collaborator/Collaborator.jsx
@@ -6,15 +6,15 @@ Collaborator.propTypes = {
     name: PropTypes.string.isRequired,
     role: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
-    primaryColor: PropTypes.string.isRequired,
+    backgroundColor: PropTypes.string.isRequired,
     onDelete: PropTypes.func
 }
 
-export default function Collaborator({ name, role, image, primaryColor, onDelete }) {
+export default function Collaborator({ name, role, image, backgroundColor, onDelete }) {
     return (
         <div className={styles.collaborator}>
             <AiFillCloseCircle size={35} onClick={onDelete} className={styles.delete} />
-            <div className={styles.header} style={{ backgroundColor: primaryColor }}>
+            <div className={styles.header} style={{ backgroundColor: backgroundColor }}>
                 <img
                     src={image}
                     alt={name}
